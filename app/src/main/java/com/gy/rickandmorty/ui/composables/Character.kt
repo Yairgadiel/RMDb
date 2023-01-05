1d207f21327b03da89fbca546084e386300e43d4
Yairgadiel <Yairgadiel@gmail.com>
2023-01-05 16:17:24 +0200

    Prettify the UI

--- app/src/main/java/com/gy/rickandmorty/ui/composables/Character.kt
+++ app/src/main/java/com/gy/rickandmorty/ui/composables/Character.kt
@@ -2,13 +2,19 @@ package com.gy.rickandmorty.ui.composables
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.gy.rickandmorty.R
 import com.gy.rickandmorty.model.entities.Location
@@ -20,18 +26,26 @@ fun Character(character: ShowCharacter? = null) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
-            .height(64.dp)
-            .padding(8.dp)
+            .wrapContentHeight()
+            .padding(16.dp)
             .fillMaxWidth()
             .background(color = colorResource(id = R.color.white))) {
         AsyncImage(
             model = character?.image,
             contentDescription = character?.name,
-            modifier = Modifier.size(50.dp)
+            modifier = Modifier
+                .size(64.dp)
+                .clip(CircleShape),
+            contentScale = ContentScale.Crop,
+            placeholder = painterResource(id = R.drawable.morty_ph)
         )
 
+        Spacer(modifier = Modifier.width(8.dp))
+
         Column(verticalArrangement = Arrangement.Center) {
-            Text(text = character?.name ?: "Name")
+            Text(text = character?.name ?: "Name",
+            style = TextStyle(fontSize = 20.sp)
+            )
             Text(text = character?.status ?: "Status")
         }
     }
